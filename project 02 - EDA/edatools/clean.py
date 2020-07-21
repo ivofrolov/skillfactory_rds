d76546f71fe92527144a903cac4effb99afb7682
Ivan Frolov <ivofrolov@fastmail.com>
2020-07-21 17:04:21 +1000

    [project 02] Correlation analysis of numeric variables

--- project 02 - EDA/edatools/clean.py
+++ project 02 - EDA/edatools/clean.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def check_anomalies(df: pd.DataFrame, actions: dict) -> dict:
-    """Check if some values are 
+    """Checks if some values are 
 
     Key of `actions` parameter is a column name, and value is a dict of any supported actions:
         `restrict`: tuple -- restrict field to a list of possible values
@@ -16,3 +16,12 @@ def check_anomalies(df: pd.DataFrame, actions: dict) -> dict:
                 counts = df.loc[:, column].value_counts()
                 result[column] = {value: counts[value] for value in diff}
     return result
+
+
+def most_common(df: pd.DataFrame, columns: list, threshold: float = .75) -> dict:
+    """Returns most common values in each of nominative variables in dataset"""
+    for column in columns:
+        sample = df[column].agg(lambda s: s.value_counts() / s.count())
+        result = sample[sample > threshold]
+        if not result.empty:
+            yield column, result.idxmax()
